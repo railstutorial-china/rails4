1fa2186b9e96eac630e1ab8d9862e2e6aad89f7a
Andor Chen <andor.chen.27@gmail.com>
2013-07-20 14:01:00 +0800

    upgrades to rails4

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,57 +1,22 @@
 var tutorialScript = {
 	init: function() {
-		var fnrefback = 'fnref-';
-
-		this.generateTOC();
-		this.enhanceFootnoteItem();
-		this.addIdToSup();
-		this.addCaptionClassToImage();
-		this.addCaptionClassToTable();
+		this.relocateNavigation();
 	},
 
-	generateTOC: function() {
-		$('.toc').tableOfContents('.main', {
-			'startLevel': 2,
-			'depth': 2
-		});
-	},
+	relocateNavigation: function() {
+		navi = $('.navigation');
+		navi_content = $('.navigation').clone();
+		if(navi.length > 0) {
+			navi.remove();
 
-	enhanceFootnoteItem: function() {
-		var t = this;
-		if($('.navigation + ol').length > 0) {
-			$('.navigation + ol').addClass('footnotes');
-
-			$('.navigation + ol > li').each(function(index, ele) {
-				var order = index+1;
-				$(ele).attr('id', 'fn-'+order);
-
-				$(ele).append('&nbsp;&nbsp;<a href="#fnref-'+order+'">&#8617;</a>');
-			});
+			if($('.footnotes').length > 0) {
+				navi_content.insertBefore('.footnotes');
+			} else {
+				navi_content.appendTo('.main');
+			}
 		}
-	},
-
-	// add an id to the footnote element in the content
-	addIdToSup: function() {
-		var $sup = $('sup', '.main'),
-		    id = 'fnref-'+$sup.text();
-		$sup.each(function(index){
-			$(this).attr('id', 'fnref-'+(index+1));
-		});
-	},
-
-	addCaptionClassToImage: function() {
-		$('img').each(function(index, ele){
-			var $caption = $(this).parent().next('p');
-			$caption.addClass('caption');
-		});
-	},
-
-	addCaptionClassToTable: function() {
-		$('table').each(function(index, ele){
-			var $caption = $(this).next('p');
-			$caption.addClass('caption');
-		})
 	}
+
 };
 
 $(document).ready(function(){
